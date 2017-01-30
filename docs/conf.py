a98556144781d69eafc10edfa88b1304ce4dbc7d
elbenfreund <elbenfreund@DenkenInEchtzeit.net>
2017-01-30 23:31:56 +0100

    Reenable autodocs
    We avoid issues with importing ``gi`` on the CI server by runing our
    ``docs`` testenv under python2.
    
    Closes: #25

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,8 @@ import hamster_gtk
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    #'sphinx.ext.autodoc',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
